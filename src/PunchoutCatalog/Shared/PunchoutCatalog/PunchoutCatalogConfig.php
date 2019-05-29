83829e30e4043b1f54c42d22c5faae35eb67c87f
Karoly Gerner <karoly.gerner@spryker.com>
2019-05-29 16:07:00 +0000

    SPRYK-9 / ps-8578 - removed invalid code

--- src/PunchoutCatalog/Shared/PunchoutCatalog/PunchoutCatalogConfig.php
+++ src/PunchoutCatalog/Shared/PunchoutCatalog/PunchoutCatalogConfig.php
@@ -7,48 +7,9 @@
 
 namespace PunchoutCatalog\Shared\PunchoutCatalog;
 
-use Generated\Shared\Transfer\ChartLayoutTransfer;
 use Spryker\Shared\Kernel\AbstractSharedConfig;
 
 class PunchoutCatalogConfig extends AbstractSharedConfig
 {
     public const VAULT_TYPE_PUNCHOUT_CATALOG_CONNECTION_PASSWORD = 'punchout-catalog-connection-password';
-
-    protected const CHART_TYPE_BAR = 'bar';
-    protected const CHART_TYPE_PIE = 'pie';
-    protected const CHART_TYPE_LINE = 'scatter';
-
-    /**
-     * @deprecated Will be removed without replacement.
-     *
-     * @return string[]
-     */
-    public function getChartTypes(): array
-    {
-        return [
-            static::CHART_TYPE_BAR,
-            static::CHART_TYPE_PIE,
-            static::CHART_TYPE_LINE,
-        ];
-    }
-
-    /**
-     * @deprecated Will be removed without replacement.
-     *
-     * @return string
-     */
-    public function getDefaultChartType(): string
-    {
-        return static::CHART_TYPE_BAR;
-    }
-
-    /**
-     * @deprecated Will be removed without replacement.
-     *
-     * @return \Generated\Shared\Transfer\ChartLayoutTransfer
-     */
-    public function getDefaultChartLayout(): ChartLayoutTransfer
-    {
-        return new ChartLayoutTransfer();
-    }
 }
