db12de453791ec1f4dbf5a2d9858712fc070856d
Dmytro Karpovych <ZAYEC77@gmail.com>
2019-06-14 14:17:56 +0300

    SPRYK-9 / ps-8578 - improve custom cart mapping

--- src/PunchoutCatalog/Yves/PunchoutCatalog/PunchoutCatalogConstants.php
+++ src/PunchoutCatalog/Yves/PunchoutCatalog/PunchoutCatalogConstants.php
@@ -9,18 +9,4 @@ namespace PunchoutCatalog\Yves\PunchoutCatalog;
 
 interface PunchoutCatalogConstants
 {
-    /**
-     * @deprecated
-     */
-    public const CUSTOM_CART_TRANSFER_MAPPING = 'CART_TRANSFER_MAPPER_DEFAULT_PLUGIN_CART_MAPPING';
-
-    /**
-     * @deprecated
-     */
-    public const CUSTOM_CART_ITEM_TRANSFER_MAPPING = 'CART_TRANSFER_MAPPER_DEFAULT_PLUGIN_CART_ITEM_MAPPING';
-
-    /**
-     * @deprecated
-     */
-    public const CUSTOM_CART_CUSTOMER_TRANSFER_MAPPING = 'CART_TRANSFER_MAPPER_DEFAULT_PLUGIN_CART_CUSTOMER_MAPPING';
 }
