d25aa2953f65807eef03442b0a2b2969874e644a
Dmytro Karpovych <ZAYEC77@gmail.com>
2019-07-04 17:08:27 +0300

    fix acceptance tests

--- tests/punchout/CheckoutProductWithPackagingUnitCept.php
+++ tests/punchout/CheckoutProductWithPackagingUnitCept.php
@@ -26,7 +26,8 @@ $i->wantTo('Add product to cart');
 $i->amOnPage('/en/Asus-HDMI-HDMI-215?attribute%5Bpackaging_unit%5D=Ring+%28500m%29');
 $i->click('[id="add-to-cart-button"]');
 $i->see('cart');
-
+$quantity = $i->getElement('[data-qa="quantity-input"]')->last()->attr('value');
+codecept_debug('Get product quantity from cart page: ' . $quantity);
 
 $i->wantTo('Transfer cart');
 
@@ -36,3 +37,4 @@ $i->seeCurrentUrlEquals('/en/punchout-catalog/cart/transfer');
 $data = $i->getBase64CxmlCartResponse();
 $i->seeCxml($data);
 $i->canSeeCxmlContains($data, '<UnitOfMeasure>EA</UnitOfMeasure>');
+$i->canSeeCxmlContains($data, "<ItemIn lineNumber=\"1\" quantity=\"$quantity\">");
